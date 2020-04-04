b1d9ccb5d818a63c46ef3d744fdf0a83c0916355
Rishabhvenkatesan <32623795+Rishabhvenkatesan@users.noreply.github.com>
2020-04-04 12:34:21 -0700

    Cave class created

---
+++ Cave Program/src/CaveMove.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Risha
+ *
+ */
+public final class CaveMove {
+
+	/**
+	 * 
+	 */
+	public CaveMove() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
